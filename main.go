58cfbdf42d3ec9fbf2c152e6210f0db0c976fc9e
邢高健 <xinggaojian@xinggaoiandeMBP.lan>
2023-02-26 00:27:57 +0800

    Gee框架的原型已经出来了

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"gee/gee"
-	"log"
 	"net/http"
 )
 
 func main() {
+	r := gee.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	})
 
-	log.Fatal(http.ListenAndServe(":9999", &gee.Engine{}))
+	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
+
+	r.Run(":9999")
 }
